128fee8dd1a68bfb2dec95901aa76e3ad0b9bd12
Peter Viegas <peterviegas@outlook.com>
2023-01-14 20:06:44 +0000

    Create parameters.ts - To allocate default values like maximum Coordenate Plateau

---
+++ src/parameters.ts
@@ -0,0 +1,11 @@
+import { Coordenation } from "./3-Interfaces";
+
+//parameters default
+export function maxCoordinatePlateau(): Coordenation{
+    //Limited for validation
+	let coodenation: Coordenation= {
+		xCoordenation:100,
+		yCoordenation:100
+	 };
+	return coodenation
+}
